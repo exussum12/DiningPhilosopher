5c7f19ff7d250d470ccadfe94664da8c92d7d0b1
Scott Dutton <scott@exussum.co.uk>
2017-11-11 21:16:26 +0000

    Mis understood number of forks

--- src/uk/co/exussum/DiningPhilosopher/Table.java
+++ src/uk/co/exussum/DiningPhilosopher/Table.java
@@ -5,13 +5,13 @@ public class Table {
     {
         int numberOfPhilosophers = 5;
         Fork[] forks = new Fork[numberOfPhilosophers + 1];
-        for(int i = 0; i <= numberOfPhilosophers; i++) {
+        for(int i = 0; i < numberOfPhilosophers; i++) {
             forks[i] = new Fork();
         }
 
         Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
         for (int i = 0; i < numberOfPhilosophers; i++) {
-            philosophers[i] = new Philosopher(i, forks[i], forks[i+1 % numberOfPhilosophers]);
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i+1) % numberOfPhilosophers]);
             Thread thread = new Thread(philosophers[i]);
             thread.start();
         }
